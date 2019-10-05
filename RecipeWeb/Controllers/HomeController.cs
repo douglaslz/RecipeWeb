639c8d89adc7548e03967a98723da8fc8ba41f22
douglaslz <44685128+douglaslz@users.noreply.github.com>
2019-10-05 18:54:55 -0400

    Add project files.

---
+++ RecipeWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/RecipeList
+        public ActionResult RecipeList()
+        {
+            return View();
+        }
+
+        // GET: Home/AddRecipe
+        public ActionResult AddRecipe()
+        {
+            return View();
+        }
+
+
+        // GET: Home/ViewRecipe
+        public ActionResult ViewRecipe()
+        {
+            return View();
+        }
+
+        // GET: Home/ReViewRecipe
+        public ActionResult ReViewRecipe()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RecipeWeb/Models/Recipes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeWeb.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Directions { get; set; }
+    }
+}
