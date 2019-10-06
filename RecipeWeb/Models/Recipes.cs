1dba8b45a2fb35949e3494637940e9a827ec9be1
douglaslz <44685128+douglaslz@users.noreply.github.com>
2019-10-05 21:36:04 -0400

    Images the images

--- RecipeWeb/Models/Recipes.cs
+++ RecipeWeb/Models/Recipes.cs
@@ -7,6 +7,7 @@ namespace RecipeWeb.Models
 {
     public class Recipes
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
